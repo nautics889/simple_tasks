47f20ab979c91b79c36c9ed1217ce5c2a5accd55
quantum <i.krokhin@quantumobile.com>
2019-01-11 11:36:31 +0200

    6320 deleted useless cycle

--- pythontask/6320_first.py
+++ pythontask/6320_first.py
@@ -5,14 +5,13 @@ def find_indexes(collection, n):
     indexes = []
 
     for i in range(len(collection)):
-        for j in range(len(custom_collection)):
-            if custom_collection[i]+custom_collection[j] == n and i != j:
-                if (j, i) not in indexes:
-                    indexes.append((i, j))
+        for j in range(len(custom_collection))[i:]:
+            if custom_collection[i] + custom_collection[j] == n and i != j:
+                indexes.append((i, j))
 
     return indexes
 
 if __name__ == '__main__':
     n = 10
-    custom_collection = [random.randint(0, 10) for _ in range(10)]
+    custom_collection = [random.randint(0, 10) for _ in range(random.randint(10, 15))]
     print(f'Collection: {custom_collection}\nIndexes: {find_indexes(custom_collection, n)}')
--- pythontask/6320_second.py
+++ pythontask/6320_second.py
@@ -5,9 +5,10 @@ def repeating_numbers(n):
     integer_list = [random.randint(0, 10) for _ in range(n)]
     print(f'List: {integer_list}')
     repeating_numbers = []
+    integer_list_range = range(len(integer_list))
 
-    for i in range(len(integer_list)):
-        for j in range(len(integer_list)):
+    for i in integer_list_range:
+        for j in integer_list_range:
             if i != j and integer_list[i] == integer_list[j] and integer_list[i] not in repeating_numbers:
                 repeating_numbers.append(integer_list[i])
 
